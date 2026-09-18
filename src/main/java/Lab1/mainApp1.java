3103c10610b2cfb81cfa3dd0cdd26d6dbe4a35d2
Vincent_C_hing <vincentching1128@gmail.com>
2026-09-18 22:23:12 +0800

    Added third print line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -8,6 +8,7 @@ public class mainApp1 {
 
         System.out.println("Program ..Starting...");
         System.out.println("1 + 1 ="+2);
+        System.out.println("another print line");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended …");
